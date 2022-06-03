ae6129ade90787de67ac96167ac92092f65e0e10
Vinesh Dodiya <dodiyavinesh352@gmail.com>
2022-06-03 19:49:42 +0530

    Bov badhu

--- client/src/app/pages/invoice-detail/invoice-detail.component.ts
+++ client/src/app/pages/invoice-detail/invoice-detail.component.ts
@@ -14,33 +14,91 @@ import { UploadService } from 'src/app/upload.service';
 })
 export class InvoiceDetailComponent implements OnInit, AfterViewInit {
   @ViewChild('imgCanvas') canvas!: ElementRef;
-  invoicePath = 'http://localhost:3000/uploads/invoice.jpg';
+  @ViewChild('selectedOption') selectedOption!: ElementRef;
   fileData: any;
-  constructor(private uploadService: UploadService) {
-    this.uploadService.fileData.subscribe((data) => {
+
+  optionsList = [
+    'Invoice Number',
+    'Senders Name',
+    'Senders Address',
+    'Phone Number',
+    'Recievers Name',
+    'Recievers Address',
+    'Invoice Date',
+    'Total Amount',
+    'Position 1',
+  ];
+
+  currentOptData = '';
+  optionsData: any = {};
+
+  serverUrl = 'http://localhost:3000/';
+  constructor(private uploadService: UploadService) {}
+  boxObj: any[] = [];
+  ngOnInit(): void {}
+  ngAfterViewInit(): void {
+    this.uploadService.fileData.subscribe((data: any) => {
+      if (!data) return;
+      const canvasSize = {
+        width: 750,
+        height: 1050,
+      };
       this.fileData = data;
+      let fileUrl = this.serverUrl + data.invoicePath.replace('\\', '/');
+      console.log(fileUrl);
+      const canvas = this.canvas.nativeElement;
+      const ctx = canvas.getContext('2d');
+      canvas.width = canvasSize.width;
+      canvas.height = canvasSize.height;
+
+      var newImg = new Image();
+
+      newImg.onload = () => {
+        var imgHeight = newImg.height;
+        var imgWidth = newImg.width;
+        canvas.style.backgroundImage = 'url(' + fileUrl + ')';
+        const boxes = data.data;
+        console.log(boxes);
+
+        for (let i = 0; i < boxes.length; i++) {
+          const element = boxes[i];
+          if (element.text == '' || element.text == '—') continue;
+          let text = element.text;
+          let left = (canvasSize.width * element.left) / imgWidth;
+          let top = (canvasSize.height * element.top) / imgHeight;
+          let boxWidth = (canvasSize.width * element.width) / imgWidth;
+          let boxHeight = (canvasSize.height * element.height) / imgHeight;
+          this.boxObj.push({ text, top, left, boxWidth, boxHeight });
+          ctx.beginPath();
+          ctx.rect(left, top, boxWidth, boxHeight);
+          ctx.stroke();
+        }
+      };
+
+      newImg.src = fileUrl;
     });
   }
 
-  ngOnInit(): void {}
-  ngAfterViewInit(): void {
-    const canvas = this.canvas.nativeElement;
-    const ctx = canvas.getContext('2d');
-    canvas.width = 500;
-    canvas.height = 700;
+  selectBox(e: any) {
+    const clickX = e.offsetX;
+    const clickY = e.offsetY;
+    for (let i = 0; i < this.boxObj.length; i++) {
+      const element = this.boxObj[i];
+      if (
+        clickX > element.left &&
+        clickX <= element.left + element.boxWidth &&
+        clickY > element.top &&
+        clickY <= element.top + element.boxHeight
+      ) {
+        console.log(element);
+        this.currentOptData += element.text + '\n';
+      }
+    }
+  }
 
-    var newImg = new Image();
-
-    newImg.onload = () => {
-      var height = newImg.height;
-      var width = newImg.width;
-      canvas.style.backgroundImage = 'url(' + this.invoicePath + ')';
-    };
-
-    newImg.src = this.invoicePath;
-
-    ctx.beginPath();
-    ctx.rect(100, 100, 200, 10);
-    ctx.stroke();
+  onAdd() {
+    this.optionsData[this.selectedOption.nativeElement.value] =
+      this.currentOptData;
+    console.log(this.optionsData);
   }
 }
