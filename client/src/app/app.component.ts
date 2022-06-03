65710f10a7b54d07d6c8f27d69459bb81d234770
Deep1218 <deeppatel18dec@gmail.com>
2022-06-03 15:59:28 +0530

    [wip] upload image and display

--- client/src/app/app.component.ts
+++ client/src/app/app.component.ts
@@ -3,8 +3,22 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
   title = 'client';
+  url = './assets/invoice.jpg';
+  onSubmit() {
+    console.log(this.url);
+  }
+  onFileSelected(e: any) {
+    console.log(e.target.files[0]);
+    if (e.target.files) {
+      let reader = new FileReader();
+      reader.readAsDataURL(e.target.files[0]);
+      reader.onload = (event: any) => {
+        this.url = event.target.result;
+      };
+    }
+  }
 }
