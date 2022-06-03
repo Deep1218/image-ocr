4a4612185cb4f64ae754675b8a0e4a186641b6be
Deep1218 <deeppatel18dec@gmail.com>
2022-06-03 16:28:03 +0530

    [feature] upload file

--- client/src/app/app.component.ts
+++ client/src/app/app.component.ts
@@ -7,18 +7,23 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'client';
-  url = './assets/invoice.jpg';
-  onSubmit() {
-    console.log(this.url);
-  }
-  onFileSelected(e: any) {
+  url: string = './assets/invoice.jpg';
+  selectedFile!: File;
+
+  previewFile(e: any) {
     console.log(e.target.files[0]);
     if (e.target.files) {
+      this.selectedFile = e.target.files[0];
       let reader = new FileReader();
       reader.readAsDataURL(e.target.files[0]);
       reader.onload = (event: any) => {
         this.url = event.target.result;
       };
     }
+  }
+  onSubmit() {
+    console.log(this.selectedFile);
+    const formData = new FormData();
+    formData.append('invoice', this.selectedFile);
   }
 }
